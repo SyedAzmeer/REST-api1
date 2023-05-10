cc5c0a9c017767d13c9e5831edb100f1aba927c2
Syed Azmeer <syed.azmeer2111@gmail.com>
2023-05-10 13:05:21 +0800

    Setup entity class

---
+++ src/main/java/com/example/crudrest/model/Employee.java
@@ -0,0 +1,25 @@
+package com.example.crudrest.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "firstname" ,nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+
+}
